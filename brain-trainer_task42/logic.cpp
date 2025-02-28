3e37e40e38540da48fe30edc9be88dabd9241aa7
Rodion Azaronak <rodionazaronak@gmail.com>
2025-02-28 12:14:03 +0300

    Add second type problems, update project

--- brain-trainer_task42/logic.cpp
+++ brain-trainer_task42/logic.cpp
@@ -6,11 +6,24 @@ int* generate_first_type_problem();
 int get_random(int a, int b) {
 	return rand() % (b - a + 1) + a;
 }
+
 int generate_brain_trainer(int amount) {
+	int start_time = time(0);
+
+	bool problem_type; // 0 or 1
+
+	for (int i = 0; i < amount; i++)
+	{
+		problem_type = get_random(0, 1);
+		if (problem_type) {
+			
+		}
+	}
+
 	return 0;
 }
 
-int* generate_first_type_problem() {
+int* generate_first_type_problem() { //a-b+c
 
 	int a, b, c;
 
@@ -21,10 +34,33 @@ int* generate_first_type_problem() {
 	} while (b >= a);
 
 	c = get_random(1, 9);
+
+
+	int numbers[]{ a,b,c };
+	return numbers;
+}
+
+int* generate_second_type_problem() { //a+b-c
+	auto a = 0;
+	auto b = 0;
+	auto c = 0;
 	
 
-	int vector[]{ a,b,c };
-	return vector;
+	a = get_random(1, 9);
+
+	do {
+		b = get_random(1, 9);
+	} while (b == a);
+
+	do {
+		c = get_random(1, 9);
+	} while (c >= (a + b) || c == a || c == b);
+
+
+
+	int numbers[]{ a,b,c };
+	return numbers;
+
 }
 
 bool is_not_unique(int* array, int length) {
--- brain-trainer_task42/main.cpp
+++ brain-trainer_task42/main.cpp
@@ -4,16 +4,23 @@ using namespace std;
 /*		v.1.0
 		Azarenok Rodion Maximovich, P13024, 15.02.2025	*/
 int* generate_first_type_problem();
+int* generate_second_type_problem();
+
 
 int main() {
 	srand(time(0));
-	for (int i = 0; i < 20; i++)
+	/*for (int i = 0; i < 10; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			cout << generate_first_type_problem()[j] << " ";
+			cout << generate_second_type_problem()[j] << (j == 0 ? " + " : j == 1 ? " - " : "");
 		}
 		cout << "\n";
-	}
+	}*/
+	/*for (int i = 0; i < 10; i++)
+	{
+		cout << generate_brain_trainer(10);
+	}*/
+
 	return 0;
 }
